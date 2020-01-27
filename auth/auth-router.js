3684c2cc1093da6ccdf6bc81b3cf8424e3b700ef
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-27 11:55:24 +0100

    wip

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-  let user = req.body;
+  const { username, password } = req.body;
+
+  const bcryptHash = bcrypt.hashSync(password, 10);
+  // generating a random salt
+  // hashing (password + randomsalt)
+  // hashing the hash etc 2 ^ 10 times!!!!!!!
+  // making the final "hash" by concatenating the following
+  // <algo><numberOfRounds><theActualSalt><theHashProper>
+  const user = {
+    username,
+    password: bcryptHash,
+  };
 
   Users.add(user)
     .then(saved => {
@@ -20,7 +31,11 @@ router.post('/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user && password === user.password) {
+      // it's taking the bcrypt thing and breaking it into parts
+      // <algo><10><foo><theactualhash>
+      // recomputes the <theactualhash>
+      // compares against the credentials req.body.password
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
