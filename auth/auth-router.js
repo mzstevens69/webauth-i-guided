dcbbc4f9841c21fc27e3e620952313ffc193a8d9
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2020-01-27 13:03:58 -0600

    guided

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,8 +2,15 @@ const router = require('express').Router();
 
 const Users = require('../users/users-model.js');
 
+const bcrypt = require('bcryptjs');
+
 router.post('/register', (req, res) => {
   let user = req.body;
+
+  const hash = bcrypt.hashSync(req.body.password, 12);
+
+  user.password = hash;
+// hash password before saving user
 
   Users.add(user)
     .then(saved => {
@@ -17,10 +24,13 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
+
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // check if password is valid
+        
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -29,6 +39,19 @@ router.post('/login', (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+router.get('/secret', (req, res, next) => {
+  if(req.headers.authorization) {
+    bcrypt.hash(req.headers.authorization, 12, (err, hash) => {
+      if(err) {
+        res.status(500).json({ err: 'oops it broke!'}); 
+      } else {
+        res.status(200).json({ hash });
+      }
+    })
+  } else {
+    res.status(400).json({ error: "missing header"})
+  }
 });
 
 module.exports = router;
